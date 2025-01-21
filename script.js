bccbd29950822afe8ead9a5d1bbb6d59e56875e3
katlegoxoxo <kmakete421@gmail.com>
2025-01-21 11:58:11 +0200

    Add script.js: Setup DOM elements and fetch data function

---
+++ script.js
@@ -0,0 +1,7 @@
+ let prompt = document.getElementById("textprompt");
+ let Picture = document.getElementById("image");
+ let ImageDiv = document.getElementById("output");
+ let Download = document.getElementById("downloadImage");
+ let CreateImg = document.getElementById("createImage");
+ 
+ let fetchData= async( URL,Headers, method,body)
